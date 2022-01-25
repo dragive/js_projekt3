5876a249e05fc2800cc39d306c91aa2f5de86b14
dragive <maciejf@int.pl>
2022-01-25 20:32:41 +0100

    cors update

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,24 @@
 const express = require("express")
 const app = express()
-const port = 3000 || process.env.port
+const port = 3001 || process.env.port
 const bodyParser = require('body-parser');
 
+const cors = require("cors")
 
 const przedmiotRouter = require('./routes/produktRoute')
 
-app.get('/', (req,res)=>{
-    res.json({message:'work!'})
-})
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/produkt',przedmiotRouter)
 
 
-app.listen(port, ()=>{
+app.get('/', (req,res)=>{
+    res.json({message:'work!'})
+})
+
+
+app.listen(port,"0.0.0.0", ()=>{
 console.log(`example app listening at http://localhost:${port}`);});
 
