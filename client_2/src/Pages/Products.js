1333822a10b7a92920ff7df522c0e22d7f1a4038
Dawid Pieciul <pieciul.dawid@gmail.com>
2022-01-29 13:36:04 +0100

    nowy wygląd

--- client_2/src/Pages/Products.js
+++ client_2/src/Pages/Products.js
@@ -1,42 +1,41 @@
-import React,  { useEffect, useState} from "react";
-import {useTable, useSortBy, useFilters} from "react-table" 
+import React, { useEffect, useState } from "react";
+import { useTable, useSortBy, useFilters } from "react-table"
 import './Components.css';
-import { BrowserRouter as useNavigate, Link  } from "react-router-dom";
+import { BrowserRouter as useNavigate, Link } from "react-router-dom";
 import ProduktService from "../ProduktService"
 // import axios from 'axios'
 
-let dane1 = {id: 1, nazwa:"Jablko", ilosc: 1, cena: 2};
-let dane2 = {id: 2, nazwa:"Gruszka", ilosc: 2, cena: 2};
+let dane1 = { id: 1, nazwa: "Jablko", ilosc: 1, cena: 2 };
+let dane2 = { id: 2, nazwa: "Gruszka", ilosc: 2, cena: 2 };
 let dane = [dane1, dane2]
-
 
 function TextFilter({
     column: { filterValue, preFilteredRows, setFilter },
-   }) {
+}) {
     const count = preFilteredRows.length
-   
-    return (
-      <input
-        value={filterValue || ''}
-        onChange={e => {
-          setFilter(e.target.value || undefined)
-        }}
-        placeholder={`Search ${count} records...`}
-      />
-    )
-   }
 
-function editElement(id){
+    return (
+        <input
+            value={filterValue || ''}
+            onChange={e => {
+                setFilter(e.target.value || undefined)
+            }}
+            placeholder={`Search ${count} records...`}
+        />
+    )
+}
+
+function editElement(id) {
     console.log(id)
     //todo
 }
-function deleteElement(id){
+function deleteElement(id) {
     console.log("ID:")
     console.log(id)
     fetch("http://localhost:3001/produkt/delete", {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ id:id }),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: id }),
         // mode: 'no-cors'
     }).then((res) => res.json())
         .then((data) => {
@@ -47,202 +46,199 @@ function deleteElement(id){
 
 }
 
-function Products(){
+function Products() {
 
 
-    const [error,setError] = useState(null)
+    const [error, setError] = useState(null)
     const [isLoaded, setIsLoaded] = useState(false)
     const [items, setItems] = useState([])
 
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("http://localhost:3001/produkt/getAll")
-        .then(res => res.json())
-        .then((result)=>{
-            console.log("result")
-            console.log(result)
-            setItems(result.data);
+            .then(res => res.json())
+            .then((result) => {
+                console.log("result")
+                console.log(result)
+                setItems(result.data);
 
 
-            console.log("is loaded")
-            setIsLoaded(true);
-            console.log(isLoaded)
+                console.log("is loaded")
+                setIsLoaded(true);
+                console.log(isLoaded)
 
 
-        }, (error)=>{
-            setIsLoaded(true);
-            setError(error);
-        })
-    },[]
-)
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            })
+    }, []
+    )
 
     console.log("123123123")
     console.log(items)
 
     const data = React.useMemo(() =>
-    
-    items ,
-    [items]
-   )
-  
-   const columns = React.useMemo(
-    () => 
-    
-    [{
-    Header: 'id',
-    accessor: 'id',
-    },
-    {
-        Header: 'nazwa',
-        accessor: 'nazwa',
-    },    
-    {
-        Header: 'ilosc',
-        accessor: 'ilosc',
-    },
-    {
-        Header: 'cena',
-        accessor: 'cena',
-    },
-    {
-        Header: 'akcje',
-        accessor: 'actions',
-        disableSortBy: true,
-        Cell: (props) =>{
-            const rowId = props.row.id
-            const id = props.row.values.id
-            return (
-                <div>
-                    <span onClick={()=>{editElement()}}>
-                        <p className="action">EDIT</p>
 
-                    </span>
+        items,
+        [items]
+    )
 
-                    <span onClick={()=>{deleteElement(id)}}>
-                            <p className="action">USUN</p>
+    const columns = React.useMemo(
+        () =>
 
-                        </span>
-                </div>
-            )
-        }
-    },
-],
-    []
-   )
+            [{
+                Header: 'Id',
+                accessor: 'id',
+            },
+            {
+                Header: 'Nazwa',
+                accessor: 'nazwa',
+            },
+            {
+                Header: 'Ilosc',
+                accessor: 'ilosc',
+            },
+            {
+                Header: 'Cena',
+                accessor: 'cena',
+            },
+            {
+                Header: 'Akcje',
+                accessor: 'actions',
+                disableSortBy: true,
+                Cell: (props) => {
+                    const rowId = props.row.id
+                    const id = props.row.values.id
+                    return (
+                        <div>
+                            <span onClick={() => { editElement() }}>
+                                <p className="przyciskFunkcyjny">Edytuj</p>
 
-   const defaultColumn = React.useMemo(
-    () => ({
-      Filter: TextFilter,
-    }),
-    []
-   )
+                            </span>
 
-   console.log("useTable")
+                            <span onClick={() => { deleteElement(id) }}>
+                                <p className="przyciskFunkcyjny">Usuń</p>
 
-   const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-   } = useTable({ columns, data ,defaultColumn,},useSortBy,)
-//useFilters
-        if(error)
-        {
-            return <div>Error: {error.message}</div>
-        }    
-        else if(!isLoaded){
-            return <div>Loading: ...</div>
-        }
-        else if(isLoaded)
-        {
-            console.log("@"+items)
-            return(
-                    <div className="produktyRamka">
-                        <div className="nazwaRamka">
-                            <div className="tekstNazwaRamka" >Zarządzanie produktami</div>
-                            <div className="margines">
-                                <table className="tabela">
-                                    <tr>
-                                        <td>
-                                            <div>
-                                                <Link to="/addproducts" className="przycisk"> Dodaj </Link>
-                                                <Link to="/modifyproduct" className="przycisk"> Modyfikuj </Link>
-                                                <Link to="/deleteproduct" className="przycisk"> Usuń </Link>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className="nazwaRamka">
-                                                <div className="tekstNazwaRamka">Wyszukiwanie</div>
-                                                <div className="margines">
-                                                    <table className="tabela">
-                                                        <tbody>
-                                                            <tr> 
-                                                                <td><input className="poleDoWpisywania" type="text" name="search"/></td> <td><button className="przyciskFunkcyjny">&lt;Szukaj&gt;</button></td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
+                            </span>
+                        </div>
+                    )
+                }
+            },
+            ],
+        []
+    )
 
-                                                </div>   
-                                            </div>
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                           ....
-                                        </td>
-                                        <td>
-                                            <br></br>
-                                            <div className="nazwaRamka">
-                                                <div className="tekstNazwaRamka">Elementy</div>
-                                                <div className="margines">
-                                                    
-                                                <table  {...getTableProps()}>
-                                                    <thead>
-                                                        {headerGroups.map(headerGroup => (
+    const defaultColumn = React.useMemo(
+        () => ({
+            Filter: TextFilter,
+        }),
+        []
+    )
+
+    console.log("useTable")
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data, defaultColumn, }, useSortBy,)
+    //useFilters
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }
+    else if (!isLoaded) {
+        return <div>Loading: ...</div>
+    }
+    else if (isLoaded) {
+        console.log("@" + items)
+        return (
+            <div className="produktyRamka">
+                <div className="nazwaRamka">
+                    <div className="tekstNazwaRamka" >Zarządzanie produktami</div>
+                    <div className="margines">
+                        <table className="tabela">
+                            <tr>
+                                <td>
+                                    <div>
+                                        <Link to="/addproducts" className="przyciskFunkcyjny"> Dodaj produkt </Link>
+                                        {/* <Link to="/modifyproduct" className="przycisk"> Modyfikuj </Link>
+                                        <Link to="/deleteproduct" className="przycisk"> Usuń </Link> */}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="nazwaRamka">
+                                        <div className="tekstNazwaRamka">Wyszukiwanie</div>
+                                        <div className="margines">
+                                            <table className="tabela">
+                                                <tbody>
+                                                    <tr>
+                                                        <td><input className="poleDoWpisywania" type="text" name="search" /></td> <td><button className="przyciskFunkcyjny">&lt;Szukaj&gt;</button></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            </table>
+                            <table className="tabela">
+                            <tr>
+                                <td>
+                                    <br></br>
+                                    <div className="nazwaRamka">
+                                        <div className="tekstNazwaRamka">Elementy</div>
+                                        <div className="margines">
+
+                                            <table className="tabela"  {...getTableProps()}>
+                                                <thead >
+                                                    {headerGroups.map(headerGroup => (
                                                         <tr {...headerGroup.getHeaderGroupProps()}>
                                                             {headerGroup.headers.map(column => (
-                                                            <th {...column.getHeaderProps(column.getSortByToggleProps())} style = {{/* #DAWID TUTAJ PIERDOLNIJ FORMATKĘ */ color: "#ffffff", }}>{column.render('Header')}
-                                                            <span>
-                                                                {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-                                                            </span>
-                                                            {/* <div>{column.canFilter ? column.render('Filter') : null}</div> */}
-                                                            </th>
-                                                            
+                                                                <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{color: "#000000", margin: "15px" }}>{column.render('Header')}
+                                                                    <span>
+                                                                        {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                                                                    </span>
+                                                                    {/* <div>{column.canFilter ? column.render('Filter') : null}</div> */}
+                                                                </th>
+
                                                             ))}
                                                         </tr>
-                                                        ))}
-                                                        </thead>
-                                                        <tbody {...getTableBodyProps()}>
-                                                            {rows.map(row => {
-                                                            prepareRow(row)
-                                                            return (
-                                                                <tr {...row.getRowProps()}>
+                                                    ))}
+                                                </thead>
+                                                <tbody {...getTableBodyProps()}>
+                                                    {rows.map(row => {
+                                                        prepareRow(row)
+                                                        return (
+                                                            <tr {...row.getRowProps()}>
                                                                 {row.cells.map(cell => {
                                                                     return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                                                                 })}
-                                                                </tr>
-                                                            )
-                                                            })}
-                                                            </tbody>
-                                                    </table>
-                                                </div>   
-                                            </div>                             
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            
-                                            <Link to="/" className="przyciskFunkcyjny"> &lt;Cofnij&gt; </Link>
-                                        </td>
-                                    </tr>
-                                </table>
-                                
-                            </div>
-                        </div>
+                                                            </tr>
+                                                        )
+                                                    })}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                    <Link to="/" className="przyciskFunkcyjny"> &lt;Cofnij&gt; </Link>
+                                </td>
+                            </tr>
+                        </table>
+
                     </div>
-           )
-        }
+                </div>
+            </div>
+        )
+    }
 }
 
 
---
+++ client_2/src/Pages/Products2.js
@@ -0,0 +1,168 @@
+import './Components.css';
+import { BrowserRouter as useNavigate, Link } from "react-router-dom";
+import axios from 'axios'
+import React, { useEffect, useState } from "react";
+
+
+
+
+function Products2() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch("http://localhost:3001/produkt/getAll")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    //console.log(result);
+
+                    setItems(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+
+
+    // useEffect(() => {
+    //     if(isLoaded)tableGeneratotr(items.data)()
+
+    // })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        console.log("page is loaded")
+    })
+
+    function tableGeneratotr(dan) {
+        var dane = dan;
+        console.log(dane)
+        window.addEventListener('DOMContentLoaded', () => {
+            console.log(dane + "#")
+
+            const tableBody = document.querySelector("#tbodyID");
+            console.log(dane + "#")
+            //console.log(window.)
+            dane.forEach((row) => {
+                const tr = document.createElement("tr");
+                const td1 = document.createElement("td");
+                td1.innerHTML = row.id;
+                tr.appendChild(td1);
+                const td2 = document.createElement("td");
+                td2.innerHTML = row.nazwa;
+                tr.appendChild(td2);
+                const td3 = document.createElement("td");
+                td3.innerHTML = row.ilosc;
+                tr.appendChild(td3);
+                const td4 = document.createElement("td");
+                td4.innerHTML = row.cena;
+                tr.appendChild(td4);
+                console.log(row);
+                tableBody.appendChild(tr);
+            })
+        });
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded || items.data == undefined) {
+        return <div>Loading...</div>;
+    } else {
+        var rows = [];
+        for (var i = 0; i < items.data.length; i++) {
+            rows.push(
+                <tr>
+                    <td>{items.data[i].id}</td>
+                    <td>{items.data[i].nazwa}</td>
+                    <td>{items.data[i].ilosc}</td>
+                    <td>{items.data[i].cena}</td>
+                </tr>
+            );
+        }
+        return (
+            <div class="produktyRamka">
+                <div class="nazwaRamka">
+                    <div class="tekstNazwaRamka" >Zarządzanie produktami</div>
+                    <div class="margines">
+                        <table class="tabela">
+                            <tr>
+                                <td>
+                                    <div>
+                                        <Link to="/addproducts" class="przycisk"> Dodaj </Link>
+                                        <Link to="/modifyproduct" class="przycisk"> Modyfikuj </Link>
+                                        <Link to="/deleteproduct" class="przycisk"> Usuń </Link>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="nazwaRamka">
+                                        <div class="tekstNazwaRamka">Wyszukiwanie</div>
+                                        <div class="margines">
+                                            <table class="tabela">
+                                                <tr>
+                                                    <td><input class="poleDoWpisywania" type="text" name="search" /></td> <td><button class="przyciskFunkcyjny">&lt;Szukaj&gt;</button></td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                </td>
+                                <td>
+                                    <br></br>
+                                    <div class="nazwaRamka">
+                                        <div class="tekstNazwaRamka">Elementy</div>
+                                        <div class="margines">
+                                            <table id="tabela" class="tabela">
+                                                <tr>
+                                                    <td>ID</td> <td>Nazwa</td> <td>Ilość</td> <td>Cena</td>
+                                                </tr>
+                                                <tbody id="tbodyID">{rows}</tbody>
+                                            </table>
+                                            <table>
+                                                <tr>
+                                                    <td><button class="przyciskStrzalka"> &lt; </button> </td> <td><button class="przyciskStrzalka"> &gt; </button> </td>
+                                                </tr>
+                                            </table>
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                    <Link to="/" class="przyciskFunkcyjny"> &lt;Cofnij&gt; </Link>
+                                </td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+                <script>{
+
+                    //tableGeneratotr(items.data)
+                    //console.log(items)
+
+
+                }</script>
+
+            </div>
+        )
+    }
+}
+
+export default Products2;
